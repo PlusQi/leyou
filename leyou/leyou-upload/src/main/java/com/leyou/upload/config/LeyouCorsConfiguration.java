65a08c3b6f9a21eb0fd12fc7856a2574d8cb0530
PlusQi <614671436@qq.com>
2020-01-30 21:49:12 +0800

    解决文件上传跨域问题

--- leyou/leyou-upload/src/main/java/com/leyou/upload/config/LeyouCorsConfiguration.java
+++ leyou/leyou-upload/src/main/java/com/leyou/upload/config/LeyouCorsConfiguration.java
@@ -1,5 +1,4 @@
 package com.leyou.upload.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
