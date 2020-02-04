a4b38fb3e4af2e26060957217f903aa475219ece
PlusQi <614671436@qq.com>
2020-02-04 21:12:19 +0800

    添加了规格参数查询方法

--- leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
@@ -1,6 +1,7 @@
 package com.leyou.item.controller;
 
 import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
 import com.leyou.item.service.SpecificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,5 +32,19 @@ public class SpecificationController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(groups);
+    }
+
+    /**
+     * 根据条件查询规格参数
+     * @param gid
+     * @return
+     */
+    @GetMapping("params")
+    public ResponseEntity<List<SpecParam>> queryParams(@RequestParam("gid")Long gid) {
+        List<SpecParam> params = this.specificationService.queryParams(gid);
+        if (CollectionUtils.isEmpty(params)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(params);
     }
 }
